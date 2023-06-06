f2d26f0e4e425637213cf4d76315df27410837a7
Oleksandr Vedmedenko <stbearua@gmail.com>
2023-06-07 00:21:37 +0200

    +1

--- src/components/ErrorMessage/ErrorMessage.jsx
+++ src/components/ErrorMessage/ErrorMessage.jsx
@@ -1,11 +1,9 @@
-import css from './ErrorMessage.module.scss';
+import css from "./ErrorMessage.module.scss";
 
 export const ErrorMessage = () => {
   return (
-    <>
-      <p className={css.errorText}>
-        Oooops, something going wrong, refresh the page
-      </p>
-    </>
+    <p className={css.errorText}>
+      Oooops, something going wrong, refresh the page
+    </p>
   );
 };
